124914761aadbae9baa09b7ad2e941e00867a851
Marializ <166574086+MzMarializ@users.noreply.github.com>
2024-04-20 01:04:50 -0400

    Add files via upload

---
+++ Services/ApplicationDbcontext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiMovil.Models;
+
+namespace MiMovil.Services
+{
+    public class ApplicationDbcontext : DbContext
+    {
+        public ApplicationDbcontext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products{ get; set; }
+
+    }
+}
